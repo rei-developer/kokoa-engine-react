b88a8099478fcd861854c79eae9f64e8d08987d9
UNYEONG <eunnamu094@gmail.com>
2019-01-01 01:11:52 +0900

    fix bug

--- backend/src/api/cloud/cloud.controller.js
+++ backend/src/api/cloud/cloud.controller.js
@@ -6,7 +6,7 @@ exports.createImage = async ctx => {
     const filename = ctx.req.file.filename
     try {
         fs.readFile(`./img/${filename}`, (err, data) => {
-            if (err) throw new Error({ status: 'fail' })
+            if (err) return ctx.body = { message: '이상한 오류', status: 'ok' }
             if (ext === '.png' || ext === '.jpg' || ext === '.jpeg') {
                 const image = sharp(data)
                 image.metadata()
@@ -15,16 +15,16 @@ exports.createImage = async ctx => {
                         .toBuffer()
                     )
                     .then(result => fs.writeFile(`./img/${filename}`, result, () => { }))
+                const thumbnail = sharp(data)
+                thumbnail.metadata()
+                    .then(() => thumbnail.resize(100, 100)
+                        .toBuffer()
+                    )
+                    .then(result => fs.writeFile(`./img/thumb/${filename}`, result, () => { }))
             }
-            const thumbnail = sharp(data)
-            thumbnail.metadata()
-                .then(() => thumbnail.resize(100, 100)
-                    .toBuffer()
-                )
-                .then(result => fs.writeFile(`./img/thumb/${filename}`, result, () => { }))
         })
         ctx.body = { filename, status: 'ok' }
     } catch (e) {
-        ctx.body = e
+        ctx.body = { message: e.message, status: 'fail' }
     }
 }
