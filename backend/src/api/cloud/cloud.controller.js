e053d22a5beeb232fc5c1bbc9806333d797518a5
UNYEONG <eunnamu094@gmail.com>
2019-01-01 01:19:09 +0900

    fix bug

--- backend/src/api/cloud/cloud.controller.js
+++ backend/src/api/cloud/cloud.controller.js
@@ -2,26 +2,23 @@ const fs = require('fs')
 const sharp = require('sharp')
 
 exports.createImage = async ctx => {
-    const ext = ctx.req.file.originalname.substr(ctx.req.file.originalname.lastIndexOf('.'), ctx.req.file.originalname.length).toLowerCase()
     const filename = ctx.req.file.filename
     try {
         fs.readFile(`./img/${filename}`, (err, data) => {
             if (err) return ctx.body = { message: '이상한 오류', status: 'ok' }
-            if (ext === '.png' || ext === '.jpg' || ext === '.jpeg') {
-                const image = sharp(data)
-                image.metadata()
-                    .then((metadata) => image.resize(Math.min(metadata.width, 960))
-                        .jpeg(80)
-                        .toBuffer()
-                    )
-                    .then(result => fs.writeFile(`./img/${filename}`, result, () => { }))
-                const thumbnail = sharp(data)
-                thumbnail.metadata()
-                    .then(() => thumbnail.resize(100, 100)
-                        .toBuffer()
-                    )
-                    .then(result => fs.writeFile(`./img/thumb/${filename}`, result, () => { }))
-            }
+            const image = sharp(data)
+            image.metadata()
+                .then((metadata) => image.resize(Math.min(metadata.width, 960))
+                    .jpeg(80)
+                    .toBuffer()
+                )
+                .then(result => fs.writeFile(`./img/${filename}`, result, () => { }))
+            const thumbnail = sharp(data)
+            thumbnail.metadata()
+                .then(() => thumbnail.resize(100, 100)
+                    .toBuffer()
+                )
+                .then(result => fs.writeFile(`./img/thumb/${filename}`, result, () => { }))
         })
         ctx.body = { filename, status: 'ok' }
     } catch (e) {
