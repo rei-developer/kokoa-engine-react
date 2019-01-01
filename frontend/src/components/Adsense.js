9902d726fd97fdf31d229e9db0e91e5bb819272a
UNYEONG <eunnamu094@gmail.com>
2019-01-01 22:07:43 +0900

    fix style ad

--- frontend/src/components/Adsense.js
+++ frontend/src/components/Adsense.js
@@ -3,24 +3,31 @@ import React from 'react'
 export default class Adsense extends React.Component {
   componentDidMount() {
     const installGoogleAds = () => {
-      const elem = document.createElement("script");
-      elem.src =
-        "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js";
-      elem.async = true;
-      elem.defer = true;
-      document.body.insertBefore(elem, document.body.firstChild);
-    };
-    installGoogleAds();
-    (window.adsbygoogle = window.adsbygoogle || []).push({});
+      const elem = document.createElement("script")
+      elem.src = "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      elem.async = true
+      elem.defer = true
+      document.body.insertBefore(elem, document.body.firstChild)
+    }
+    installGoogleAds()
+      (window.adsbygoogle = window.adsbygoogle || []).push({})
   }
 
   render() {
+    const container = {
+      maxWidth: 970,
+      margin: '0 auto .5rem auto'
+    }
     return (
-      <ins className='adsbygoogle'
-        style={{ display: 'block' }}
-        data-ad-client='ca-pub-5633529273423665'
-        data-ad-slot='1882412178'
-        data-ad-format='auto' />
-    );
+      <div style={container}>
+        <ins
+          className='adsbygoogle'
+          style={{ display: 'block' }}
+          data-ad-client='ca-pub-5633529273423665'
+          data-ad-slot='1882412178'
+          data-ad-format='auto'
+        />
+      </div>
+    )
   }
 }
