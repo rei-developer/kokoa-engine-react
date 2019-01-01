e27ef14fc087088be5553507eedb9636a1424614
UNYEONG <eunnamu094@gmail.com>
2019-01-01 18:33:18 +0900

    f

--- frontend/src/components/Adsense.js
+++ frontend/src/components/Adsense.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 export default class Adsense extends React.Component {
   componentDidMount() {
     const installGoogleAds = () => {
@@ -15,10 +17,10 @@ export default class Adsense extends React.Component {
   render() {
     return (
       <ins className='adsbygoogle'
-           style={{ display: 'block' }}
-           data-ad-client='ca-pub-12121212'
-           data-ad-slot='12121212'
-           data-ad-format='auto' />
+        style={{ display: 'block' }}
+        data-ad-client='ca-pub-5633529273423665'
+        data-ad-slot='1882412178'
+        data-ad-format='auto' />
     );
   }
 }
