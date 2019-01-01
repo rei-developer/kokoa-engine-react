929e6a9b504ba42cfcababf23c61fd46a5e701d5
UNYEONG <eunnamu094@gmail.com>
2019-01-01 18:10:30 +0900

    fix

--- frontend/src/components/Adsense.js
+++ frontend/src/components/Adsense.js
@@ -2,18 +2,24 @@ import React from 'react'
 
 export default class Adsense extends React.Component {
   componentDidMount() {
-    (window.adsbygoogle = window.adsbygoogle || []).push({})
+    $(document).ready(() => { (adsbygoogle = window.adsbygoogle || []).push({}) })
   }
 
   render() {
+    const container = {
+      display: 'block',
+      maxWidth: 970,
+      margin: '0 auto .5rem auto'
+    }
     return (
-      <ins
-        className='adsbygoogle'
-        style={{ display: 'block', maxWidth: 970, margin: '0 auto .5rem auto' }}
-        data-ad-client='ca-pub-5633529273423665'
-        data-ad-slot='1882412178'
-        data-ad-format='auto'
-      />
+      <div style={container}>
+        <ins
+          className='adsbygoogle'
+          data-ad-client='ca-pub-5633529273423665'
+          data-ad-slot='1882412178'
+          data-ad-format='auto'
+        />
+      </div>
     )
   }
 }
