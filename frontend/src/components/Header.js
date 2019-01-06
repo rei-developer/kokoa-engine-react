abc9e2e125c155b29362b88953c3da8bec7f61ef
Unyeong <eunnamu094@gmail.com>
2019-01-06 21:26:49 +0900

    try

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -29,7 +29,7 @@ import { observer, inject } from 'mobx-react'
 import Logo from '../Logo.png'
 import GirlLogo from '../GirlLogo.png'
 
-const VERSION = 15
+const VERSION = 16
 
 const theme = createMuiTheme({
   typography: {
