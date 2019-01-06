70a752a68474e458e241e2acff64fa32b37c260a
Unyeong <eunnamu094@gmail.com>
2019-01-06 20:49:09 +0900

    cache test

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -29,7 +29,7 @@ import { observer, inject } from 'mobx-react'
 import Logo from '../Logo.png'
 import GirlLogo from '../GirlLogo.png'
 
-const VERSION = 7
+const VERSION = 8
 
 const theme = createMuiTheme({
   typography: {
