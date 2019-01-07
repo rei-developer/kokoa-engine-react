822b2021660781869b45327cd99205b151693554
Unyeong <eunnamu094@gmail.com>
2019-01-07 23:46:38 +0900

    fix update

--- updater/src/index.js
+++ updater/src/index.js
@@ -3,7 +3,7 @@ const client = redis.createClient()
 const schedule = require('node-schedule')
 const updateTopic = require('./database/update')
 
-schedule.scheduleJob('00 00 * * * *', () => {
+schedule.scheduleJob('00 00 05 * * *', () => {
   client.keys('*', async (err, keys) => {
     if (err) return
     const jobs = keys.map(key => new Promise(resolve => {
